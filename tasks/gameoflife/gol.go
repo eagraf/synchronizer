d55683e3a76bc0a81a0b3f1dafa7315930dee226
ethan <graf.ethan@gmail.com>
2020-04-18 22:33:55 -0400

    World size: 1024

--- tasks/gameoflife/gol.go
+++ tasks/gameoflife/gol.go
@@ -1,6 +1,8 @@
 package gameoflife
 
 import (
+	"math/rand"
+
 	"github.com/eagraf/synchronizer/tasks"
 )
 
@@ -27,6 +29,7 @@ func setup(intent *tasks.Intent) (*tasks.TaskInstance, []*tasks.Intent) {
 	// Initialize partial results and intents
 	partialResults := make([]interface{}, intent.Config.NumWorkers)
 	mapIntents := make([]*tasks.Intent, intent.Config.NumWorkers)
+	board := generateRandomBoard(1024)
 	for i := range partialResults {
 		partialResults[i] = GOLPartialResult{
 			Data: "Hello",
@@ -38,7 +41,7 @@ func setup(intent *tasks.Intent) (*tasks.TaskInstance, []*tasks.Intent) {
 			Config:     intent.Config,
 			Input: map[string]interface{}{
 				"size": 8,
-				"board": []int8{
+				/*"board": []int8{
 					0, 0, 0, 0, 0, 0, 0, 0,
 					0, 0, 0, 0, 0, 0, 0, 0,
 					0, 0, 0, 0, 1, 0, 0, 0,
@@ -47,7 +50,8 @@ func setup(intent *tasks.Intent) (*tasks.TaskInstance, []*tasks.Intent) {
 					0, 0, 0, 0, 0, 0, 0, 0,
 					0, 0, 0, 0, 0, 0, 0, 0,
 					0, 0, 0, 0, 0, 0, 0, 0,
-				},
+				},*/
+				"board": board,
 			},
 		}
 	}
@@ -69,4 +73,14 @@ func setup(intent *tasks.Intent) (*tasks.TaskInstance, []*tasks.Intent) {
 func reduce() *tasks.Intent {
 	endIntent := &tasks.Intent{}
 	return endIntent
+}
+
+func generateRandomBoard(n int) []int8 {
+	board := make([]int8, n*n)
+	for y := 0; y < n; y++ {
+		for x := 0; x < n; x++ {
+			board[(n*y)+x] = int8(rand.Int() % 2)
+		}
+	}
+	return board
 }
