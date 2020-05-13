a7e49409c234aa576be4788f87fe51831bfefa91
ethan <graf.ethan@gmail.com>
2020-05-13 00:16:12 -0400

    Pubsub implementation

--- messenger/pubsub.go
+++ messenger/pubsub.go
@@ -1,11 +1,13 @@
 package messenger
 
+import "errors"
+
 // Subscriber is an interface implemented by routines that need to listen for specific messages
 type Subscriber interface {
-	GetIdentifier() string                                   // Used to identify if the subscriber is already applied to a topic
-	OnReceive(topic string, message *map[string]interface{}) // Callback after publish on message received
-	OnSend(topic string, message *map[string]interface{})    // Callback after publish on message sent
-	OnClose(topic string)                                    // Callback after topic closed
+	GetIdentifier() string                    // Used to identify if the subscriber is already applied to a topic
+	OnReceive(topic string, message *Message) // Callback after publish on message received
+	OnSend(topic string, message *Message)    // Callback after publish on message sent
+	OnClose(topic string)                     // Callback after topic closed
 }
 
 // PubSub handles notifying all relevant subroutines about incoming messages
@@ -22,33 +24,65 @@ func newPubSub() *PubSub {
 }
 
 // AddSubscription applies a Subscriber to a topic
-func (ps *PubSub) AddSubscription(topic string, subscriber Subscriber) {
-
+func (ps *PubSub) AddSubscription(topic string, subscriber Subscriber) error {
+	if _, ok := ps.subs[topic]; ok == false {
+		return errors.New("Topic does not exist")
+	}
+	ps.subs[topic][subscriber.GetIdentifier()] = &subscriber
+	return nil
 }
 
 // RemoveSubscription removes a Subscriber from a topic
-func (ps *PubSub) RemoveSubscription(topic string, subscriberID string) {
+func (ps *PubSub) RemoveSubscription(topic string, subscriberID string) error {
+	if _, ok := ps.subs[topic]; ok == false {
+		return errors.New("Topic does not exist")
+	}
 
+	if _, ok := ps.subs[topic][subscriberID]; ok == true {
+		delete(ps.subs[topic], subscriberID)
+	}
+	return nil
 }
 
 // TODO investigate if addTopic and closeTopic should be publicly available
 
 // Create a new topic
 func (ps *PubSub) addTopic(topic string) {
-
+	if _, ok := ps.subs[topic]; ok == false {
+		ps.subs[topic] = make(map[string]*Subscriber)
+	}
 }
 
 // Close a topic
 func (ps *PubSub) closeTopic(topic string) {
-
+	if _, ok := ps.subs[topic]; ok == true {
+		for _, sub := range ps.subs[topic] {
+			(*sub).OnClose(topic)
+		}
+		delete(ps.subs, topic)
+	}
 }
 
 // Notify all relevant subscribers about a message received
-func (ps *PubSub) publishReceived(topic string, message *Message) {
+func (ps *PubSub) publishReceived(topic string, message *Message) error {
+	if _, ok := ps.subs[topic]; ok == false {
+		return errors.New("Topic does not exist")
+	}
 
+	for _, sub := range ps.subs[topic] {
+		(*sub).OnReceive(topic, message)
+	}
+	return nil
 }
 
 // Notify all relevant subscribers about a message sent
-func (ps *PubSub) publishSend(topic string, message *Message) {
+func (ps *PubSub) publishSend(topic string, message *Message) error {
+	if _, ok := ps.subs[topic]; ok == false {
+		return errors.New("Topic does not exist")
+	}
 
+	for _, sub := range ps.subs[topic] {
+		(*sub).OnSend(topic, message)
+	}
+	return nil
 }
