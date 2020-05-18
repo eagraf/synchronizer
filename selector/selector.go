046cf26b1c009bf69a7bf495c0b1645ab43215aa
ethan <graf.ethan@gmail.com>
2020-05-18 13:04:16 -0400

    Selector tests and updated specification

--- selector/selector.go
+++ selector/selector.go
@@ -7,34 +7,46 @@ import (
 	"net/rpc"
 	"strconv"
 
+	"github.com/eagraf/synchronizer/messenger"
 	"github.com/go-chi/chi"
 )
 
-type selector struct {
-	workers []worker // Don't use a list of pointers so that workers can be easilly encoded
+const (
+	// MessageRegistrationResponse is a message type identifier for selector registration responses
+	MessageRegistrationResponse string = "selector_registration_response"
+	// MessageHealthCheck is a message type identifier for selector health checks
+	MessageHealthCheck string = "selector_health_check"
+	// MessageHandoff is a message type identifier for selector handoffs
+	MessageHandoff string = "selector_handoff"
+)
+
+type Selector struct {
+	workers []Worker // Don't use a list of pointers so that workers can be easilly encoded
 }
 
-type worker struct {
-	UUID string
+type Worker struct {
+	UUID         string
+	Healthy      bool
+	Disconnected bool
 }
 
-type workerRequest struct {
+type WorkerRequest struct {
 	numRequested int
 }
 
-type workerReply struct {
-	workers []worker
+type WorkerResponse struct {
+	workers []Worker
 }
 
-type handoffRequest struct {
+type HandoffRequest struct {
 }
 
-type handoffReply struct {
+type HandoffResponse struct {
 }
 
-func newSelector(apiPort int, rpcPort int) (*selector, error) {
+func newSelector(apiPort int, rpcPort int) (*Selector, error) {
 	// Initialize selector
-	var s *selector = new(selector)
+	var s *Selector = new(Selector)
 
 	// Start api
 	routes := registerRoutes(s)
@@ -60,7 +72,7 @@ func newSelector(apiPort int, rpcPort int) (*selector, error) {
 }
 
 // RegisterRoutes defines routes for REST API using the chi router
-func registerRoutes(s *selector) http.Handler {
+func registerRoutes(s *Selector) http.Handler {
 
 	r := chi.NewRouter()
 	r.Route("/websocket", func(r chi.Router) {
@@ -70,14 +82,52 @@ func registerRoutes(s *selector) http.Handler {
 }
 
 // HTTP endpoint that promotes an HTTP request to a full WebSocket connection
-func (s *selector) websocket(w http.ResponseWriter, r *http.Request) {
+func (s *Selector) websocket(w http.ResponseWriter, r *http.Request) {
 }
 
 // RPC interface
-func (s *selector) GetWorkers(request workerRequest, reply *workerReply) error {
+func (s *Selector) GetWorkers(request WorkerRequest, reply *WorkerResponse) error {
 	return nil
 }
 
-func (s *selector) Handoff(request handoffRequest, reply *handoffRequest) error {
+func (s *Selector) Handoff(request HandoffRequest, reply *HandoffRequest) error {
+	return nil
+}
+
+func (s *Selector) GetIdentifier() {
+
+}
+
+func (s *Selector) OnReceive(topic string, message *messenger.Message) {
+
+}
+
+/*
+ * Types of sends:
+ *   (1) Registration Response (selector_registration_response)
+ *   (2) Health Check (selector_health_check)
+ *   (3) Handoff (selector_handoff)
+ */
+func (s *Selector) OnSend(topic string, message *messenger.Message) {
+
+}
+
+func (s *Selector) OnClose(topic string) {
+
+}
+
+func (s *Selector) sendRegistrationResponse() {
+
+}
+
+func (s *Selector) sendHealthCheck() {
+
+}
+
+func (s *Selector) sendHandoff() {
+
+}
+
+func (s *Selector) getWorker(workerUUID string) *Worker {
 	return nil
 }
--- selector/selector_test.go
+++ selector/selector_test.go
@@ -2,15 +2,28 @@ package selector
 
 import (
 	"net/http"
+	"os"
 	"testing"
+	"time"
+
+	"github.com/eagraf/synchronizer/messenger"
 )
 
-func TestNewSelector(t *testing.T) {
-	_, err := newSelector(3000, 3001)
-	if err != nil {
-		t.Error("No error expected from starting selector: " + err.Error())
-	}
+// Selector global variable
+var globalSelector *Selector
 
+// Setup common server for all tests to use
+func TestMain(m *testing.M) {
+	s, err := newSelector(3000, 3001)
+	if err != nil {
+		// Server failed to start
+		os.Exit(-1)
+	}
+	globalSelector = s
+	os.Exit(m.Run())
+}
+
+func TestNewSelector(t *testing.T) {
 	// Test that external API started
 	req, err := http.NewRequest("GET", "http://localhost:3000", nil)
 	if err != nil {
@@ -30,4 +43,155 @@ func TestNewSelector(t *testing.T) {
 	if err != nil {
 		t.Error("Request failed: " + err.Error())
 	}
+}
+
+func TestWorkerRegistration(t *testing.T) {
+	tc, _ := messenger.NewTestClient("http://localhost:3000/websocket/", "test-client-id")
+
+	// Receive response on client
+	_, err := tc.Receive()
+	if err != nil {
+		t.Error("Error receiving response from selector: " + err.Error())
+	}
+
+	// Receive connection response
+	m2, err := tc.Receive()
+	if err != nil {
+		t.Error("Error recieved instead of registration response: " + err.Error())
+	}
+	if m2.GetMetadata().MessageType != MessageRegistrationResponse {
+		t.Error("Incorrect message type")
+	}
+	// Test message contents
+	id, ok := m2.GetHeader("session_id")
+	if ok == false {
+		t.Error("No session_id header")
+	}
+	if len(id.(string)) != 36 {
+		t.Error("id not valid UUID")
+	}
+}
+
+func TestHealthCheck(t *testing.T) {
+	tc, _ := messenger.NewTestClient("http://localhost:3000/websocket/", "test-client-id")
+
+	// Receive response on client
+	_, err := tc.Receive()
+	if err != nil {
+		t.Error("Error receiving response from selector: " + err.Error())
+	}
+
+	// Receive connection response
+	_, err = tc.Receive()
+	if err != nil {
+		t.Error("Error recieved instead of registration response: " + err.Error())
+	}
+
+	// Send healthcheck
+	globalSelector.sendHealthCheck()
+	hc, err := tc.Receive()
+	if err != nil {
+		t.Error("Error recieved instead of registration response: " + err.Error())
+	}
+	if hc.GetMetadata().MessageType != MessageHealthCheck {
+		t.Error("Incorrect message type")
+	}
+
+	// Send response to healthcheck
+	mb := new(messenger.MessageBuilder)
+	m, _ := mb.NewMessage("worker_health_check_response", hc.GetMetadata().Request).Done()
+	err = tc.Send(m)
+	if err != nil {
+		t.Error("Error sending health check response: " + err.Error())
+	}
+
+	// Check worker status in selector
+	if globalSelector.getWorker("test-client-id").Healthy == false {
+		t.Error("Worker is not healthy")
+	}
+}
+
+func TestHealthCheckTimeout(t *testing.T) {
+	tc, _ := messenger.NewTestClient("http://localhost:3000/websocket/", "test-client-id")
+
+	// Receive response on client
+	_, err := tc.Receive()
+	if err != nil {
+		t.Error("Error receiving response from selector: " + err.Error())
+	}
+
+	// Receive connection response
+	_, err = tc.Receive()
+	if err != nil {
+		t.Error("Error recieved instead of registration response: " + err.Error())
+	}
+
+	// Send healthcheck
+	globalSelector.sendHealthCheck()
+	hc, err := tc.Receive()
+	if err != nil {
+		t.Error("Error recieved instead of registration response: " + err.Error())
+	}
+	if hc.GetMetadata().MessageType != MessageHealthCheck {
+		t.Error("Incorrect message type")
+	}
+
+	time.Sleep(5 * time.Second)
+
+	// Check worker status in selector
+	if globalSelector.getWorker("test-client-id").Healthy == true {
+		t.Error("Worker is healthy")
+	}
+}
+
+func TestWorkerDisconnect(t *testing.T) {
+	tc, _ := messenger.NewTestClient("http://localhost:3000/websocket/", "test-client-id")
+
+	// Receive response on client
+	_, err := tc.Receive()
+	if err != nil {
+		t.Error("Error receiving response from selector: " + err.Error())
+	}
+
+	// Receive connection response
+	_, err = tc.Receive()
+	if err != nil {
+		t.Error("Error recieved instead of registration response: " + err.Error())
+	}
+
+	err = tc.Close()
+	if err != nil {
+		t.Error("Error closing client: " + err.Error())
+	}
+
+	// Check worker status in selector
+	if globalSelector.getWorker("test-client-id").Disconnected == false {
+		t.Error("Worker is not disconnected")
+	}
+
+}
+
+func TestRPCGetWorkers(t *testing.T) {
+	// Connect three clients
+	messenger.NewTestClient("http://localhost:3000/websocket/", "client1")
+	messenger.NewTestClient("http://localhost:3000/websocket/", "client2")
+	messenger.NewTestClient("http://localhost:3000/websocket/", "client3")
+
+	req := WorkerRequest{}
+	res := new(WorkerResponse)
+	err := globalSelector.GetWorkers(req, res)
+	if err != nil {
+		t.Error("RPC failed: " + err.Error())
+	}
+
+	if len(res.workers) < 3 {
+		t.Error("Incorrect number of workers")
+	}
+
+}
+
+// TODO testing handoff will be difficult because it requires interactions with other services
+
+func TestRPCHandoff(t *testing.T) {
+
 }
