0e3195554f0dc46ecb8eaaa45ff350228c5dcbda
ethan <graf.ethan@gmail.com>
2020-05-19 09:18:22 -0400

    Completed requests are moved into separate structure

--- messenger/log.go
+++ messenger/log.go
@@ -1,64 +1,90 @@
 package messenger
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
-// TODO currently the roundtrip request/response may not resemble all patterns of communication. A better system for interlinking messages may be needed (graph?)
-type log struct {
-	openRequests  map[string]*roundTrip
-	requestBuffer []*RoundTrip // TODO this will grow out of control -> a better logging system is needed
-	logger        log.Logger
+type messengerLog struct {
+	openRequests      map[string]*RoundTrip
+	completedRequests map[string]*RoundTrip // TODO this will grow out of control -> a better logging system is needed
 }
 
 // RoundTrip represents a request response pair
+// TODO This might not encapsulate all possible messaging patterns, a more general graph based structure might be needed
 type RoundTrip struct {
 	requestID string
 	request   *Message
 	response  *Message
 }
 
-// Duration gets the total duration of a request
-func (rt *RoundTrip) Duration() time.Duration {
-	return time.Parse(time.StampMilli, rt.response.metadata.Timestamp).
-		Sub(time.Parse(time.StampMilli, rt.request.metadata.Request.metadata.Timestamp)).
-		Format(time.StampMilli)
+// Create a new instance of a messenger log
+func newMessengerLog() *messengerLog {
+	ml := messengerLog{
+		openRequests:      make(map[string]*RoundTrip),
+		completedRequests: make(map[string]*RoundTrip),
+	}
+	return &ml
 }
 
-func (l *log) GetIdentifier() string {
+// Duration gets the total duration of a request
+func (rt *RoundTrip) Duration() (*time.Duration, error) {
+	if rt.request == nil || rt.response == nil {
+		return nil, errors.New("RoundTrip struct incomplete")
+	}
+	t1, err1 := time.Parse(time.StampMilli, rt.response.metadata.Timestamp)
+	t2, err2 := time.Parse(time.StampMilli, rt.request.metadata.Timestamp)
+	if err1 != nil {
+		return nil, err1
+	}
+	if err2 != nil {
+		return nil, err2
+	}
+	d := t2.Sub(t1)
+	return &d, nil
+}
+
+func (l *messengerLog) GetIdentifier() string {
 	// TODO universal identifier
 	return "messenger_logger"
 }
 
 // OnReceive callback insert or complete roundtrip
-func (l *log) OnReceive(topic string, message *Message) {
+func (l *messengerLog) OnReceive(topic string, message *Message) {
 	// Check if it matches any active message request
-	rt, ok := l.openRequests[topic]
+	rt, ok := l.openRequests[message.metadata.Request]
 	if ok == true {
 		// Complete round trip
 		rt.response = message
+		l.completedRequests[rt.requestID] = rt
+		delete(l.openRequests, rt.requestID)
 	} else {
 		// Otherwise create new request
 		rt = new(RoundTrip)
 		rt.requestID = message.metadata.Request
+		rt.request = message
 		l.openRequests[rt.requestID] = rt
 	}
-
 }
 
 // OnSend callback insert or complete roundtrip
-func (l *Log) OnSend(topic string, message *Message) {
+func (l *messengerLog) OnSend(topic string, message *Message) {
 	// Check if it matches any active message request
-	rt, ok := l.openRequests[topic]
+	rt, ok := l.openRequests[message.metadata.Request]
 	if ok == true {
 		// Complete round trip
 		rt.response = message
+		l.completedRequests[rt.requestID] = rt
+		delete(l.openRequests, rt.requestID)
 	} else {
 		// Otherwise create new request
 		rt = new(RoundTrip)
 		rt.requestID = message.metadata.Request
+		rt.request = message
 		l.openRequests[rt.requestID] = rt
 	}
 }
 
-func (l *Log) OnClose(topic string) {
+func (l *messengerLog) OnClose(topic string) {
 	// pass
 }
---
+++ messenger/log_test.go
@@ -0,0 +1,87 @@
+package messenger
+
+import "testing"
+
+func TestStartRoundtrip(t *testing.T) {
+	l := newMessengerLog()
+	ps := newPubSub()
+
+	ps.addTopic("new-topic")
+
+	ps.AddSubscription("new-topic", l)
+
+	mb := MessageBuilder{}
+	m, _ := mb.NewMessage("new-message", "request-id").Done()
+
+	ps.publishSend("new-topic", m)
+	rt, ok := l.openRequests["request-id"]
+	if ok == false {
+		t.Error("New roundtrip is missing")
+	}
+	if rt.request == nil {
+		t.Error("Request should be filled")
+	}
+	if rt.response != nil {
+		t.Error("Response should be nil")
+	}
+
+	mb = MessageBuilder{}
+	m, _ = mb.NewMessage("new-message", "request-id").Done()
+	ps.publishReceived("new-topic", m)
+	rt, ok = l.completedRequests["request-id"]
+	if rt.request == nil {
+		t.Error("Request should be filled")
+	}
+	if rt.response == nil {
+		t.Error("Response should be filled")
+	}
+
+	if len(l.openRequests) != 0 {
+		t.Error("Length of openRequests should be 0")
+	}
+	if len(l.completedRequests) != 1 {
+		t.Error("Length of openRequests should be 0")
+	}
+}
+
+func TestEndRoundtrip(t *testing.T) {
+	l := newMessengerLog()
+	ps := newPubSub()
+
+	ps.addTopic("new-topic")
+
+	ps.AddSubscription("new-topic", l)
+
+	mb := MessageBuilder{}
+	m, _ := mb.NewMessage("new-message", "request-id").Done()
+	ps.publishReceived("new-topic", m)
+	rt, ok := l.openRequests["request-id"]
+	if rt.request == nil {
+		t.Error("Request should be filled")
+	}
+	if rt.response != nil {
+		t.Error("Response should be missing")
+	}
+
+	mb = MessageBuilder{}
+	m, _ = mb.NewMessage("new-message", "request-id").Done()
+
+	ps.publishSend("new-topic", m)
+	rt, ok = l.completedRequests["request-id"]
+	if ok == false {
+		t.Error("New roundtrip is missing")
+	}
+	if rt.request == nil {
+		t.Error("Request should be filled")
+	}
+	if rt.response == nil {
+		t.Error("Response should be filled")
+	}
+
+	if len(l.openRequests) != 0 {
+		t.Error("Length of openRequests should be 0")
+	}
+	if len(l.completedRequests) != 1 {
+		t.Error("Length of openRequests should be 0")
+	}
+}
